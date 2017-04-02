fc3367886da387cc8f024f68f1261068caa7c4f5
Kirill Borzov <oresh.work@gmail.com>
2017-04-02 12:05:04 +0300

    issue: more stuff.

--- js/options.js
+++ js/options.js
@@ -1,5 +1,97 @@
 var main_tabs = new Tabs('#main_tabs');
 
 document.addEventListener('optionsPageReady', function () {
-    /* Options page is ready. Write your code here */
+  /* Options page is ready. Write your code here */
+  (function($) {
+    //storage.area.set({'book':'hello!'});
+
+    function getGreetingText(greetings, prewishes, wishes) {
+      var smiles = [
+      '😘','🙂','😊','☺','😄','😁','😃','😀','😎','🤠','😺','😸',
+      '😼','👏','👍','🍰','🎂','🍮','🍭','🍬','🍫','🍩','🍪','🍧',
+      '🍨','🍦','🥂','🍺','🍷','🍹','🍾','🏅','🎸','🎷','❗']
+
+      function getGreeting() {
+        return greetings[Math.floor(Math.random() * greetings.length)]
+      }
+
+      function getPrewish() {
+        return prewishes[Math.floor(Math.random() * prewishes.length)];
+      }
+
+      function getWish(number_wishes, separators) {
+        return getItemsFromArray(wishes, number_wishes, separators)
+      }
+
+      function getSmiles(number_smiles, separators) {
+        return getItemsFromArray(smiles, number_smiles, separators)
+      }
+
+      function getItemsFromArray(array, number_of_items, separators) {
+        var localArr = Array.from(array);
+        var text = ''
+        for (var i = 0; i < number_of_items; i++) {
+          var ind = Math.floor(Math.random() * localArr.length);
+          text += localArr[ind];
+          if (separators && separators[i]) {
+            text += separators[i];
+          }
+          localArr.splice(ind, 1);
+        }
+        return text;
+      }
+
+      text = getGreeting() + '\n' + getPrewish() + ' ';
+      text += getWish(3, [', ', ' and ', '! ']);
+      text += ' ' + getSmiles(3);
+      return text;
+    }
+    
+    function save_options() {
+      var wishes = $('#wishes').val();
+      var prewishes = $('#prewishes').val();
+      var greetings = $('#greetings').val();
+
+      chrome.storage.sync.set({
+        wishes: wishes,
+        prewishes : prewishes,
+        greetings : greetings
+      }, function() {
+        // Update status to let user know options were saved.
+        console.log('changes saved');
+      });
+    }
+
+    function restore_options() {
+      // Use default value color = 'red' and likesColor = true.
+      chrome.storage.sync.get({
+        wishes: wishesLocalStorage.default_options.wishes,
+        prewishes : wishesLocalStorage.default_options.prewishes,
+        greetings : wishesLocalStorage.default_options.greetings
+      }, function(items) {
+
+        var wishes = $('#wishes').val(items.wishes);
+        var prewishes = $('#prewishes').val(items.prewishes);
+        var greetings = $('#greetings').val(items.greetings);
+        text = getGreetingText(items.greetings.split('\n'), items.prewishes.split('\n'), items.wishes.split('\n'));
+        $('#example').html(text);
+      });
+    }
+
+    restore_options();
+
+    $('.reset-btn').click(function() {
+      if (confirm('Are you sure you want to reset to defualts?')) {
+        $('#wishes').val(wishesLocalStorage.default_options.wishes);
+        $('#prewishes').val(wishesLocalStorage.default_options.prewishes);
+        $('#greetings').val(wishesLocalStorage.default_options.greetings);
+        save_options();
+      }
+    });
+
+    $('textarea').on('keyup',function() {
+      save_options();
+    });
+
+  })(jQuery);
 });
--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,37 @@
-var storage = {
+window.wishesLocalStorage = {
     // `sync` if you want. remember about QUOTAS https://developer.chrome.com/extensions/storage.html#sync-properties
-    area: chrome.storage.local,
     default_options: {
+    	wishes : [
+			"to enjoy the happy moments of life and share them with your loved ones",
+			"to always begin your day with a smile in your soul",
+			"friends that will be there for you in the time of need and joy",
+			"colored emotions every day",
+			"to have as many as possible meaningful life experiences on your path",
+			"to meet as many awesome and inspiring people as you can",
+			"to make your dreams become a reality",
+			"to taste life in it's many varieties it comes",
+			"lots of hugs, kisses and adventures",
+			"sunny days full of joy and laughs",
+			"to climb each day at least one step to make your life and others a bit better",
+			"to continue doing what you love and do best",
+			"to experience adventures that you will later on tell your grandchildren ",
+			"say Yes as often as possible to new adventures",
+			"to take any travel opportunity life offers You",
+			"to never forget that You have to get older, but you don't have to grow up"
+		].join('\n'),
+    	prewishes : [
+			"On this amazing day I wish You",
+			"Without further ado, I wish You",
+			"For this day and the next to be 364 awesome I wish You",
+		].join('\n'),
+    	greetings : [
+			"Happy Birthday with cookies!",
+			"Hey, It's Your Birthday, It's Your Birthday, We're gonna party like it's Your Birthday!",
+			"On this cheerful and awesome day, I say Happy Birthday!",
+			"They say it's an awesome day, because it's Your Birthday!",
+			"The time has come, Happy Birthday to You!"
+		].join('\n'),
+    	invite : 0
         // one key for every option
     }
 };
